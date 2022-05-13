de2017cf0abb7e2e9d73d32dedd8e6621b5add36
Melissa Tapia <mtapia.dpl@gmail.com>
2022-05-13 10:35:57 -0500

    played with TS solved add row broke all func

---
+++ javascript_typescript_6_4/competency_project/todolist.js
@@ -0,0 +1,24 @@
+var theNewWordString = document.forms["myForm"]["addToList"].value;
+var theNewWord;
+function validateANDAdd() {
+    //set function (method) variables
+    if (theNewWord == " ") {
+        //word was not entered; let user know
+        alert("Please enter a word");
+        return false;
+    }
+    //adds users entry to list
+    else {
+        var table = document.getElementById("runningList");
+        var row = table.insertRow(0);
+        (table.insertRow(table.rows.length)).innerHTML = theNewWord;
+    }
+    //erase form fields
+    document.forms["myForm"]["newWord"].value = " ";
+    document.forms["myForm"]["newNumber"].value = " ";
+    return true;
+}
+function deleteToDoList() {
+    //clear table of all rows
+    var clearList1 = document.forms["wordList"].value = "";
+}
