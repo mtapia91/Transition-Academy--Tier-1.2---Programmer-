1280c296f5bf6126e09bdd2d4018445a4b3d4a30
Melissa Tapia <mtapia.dpl@gmail.com>
2022-05-20 16:36:15 -0500

    fucked it up

--- frontend_foundation_pt2_w7/competency_bootstrap_moviequote_d2/moviequote.js
+++ frontend_foundation_pt2_w7/competency_bootstrap_moviequote_d2/moviequote.js
@@ -1,24 +1,56 @@
-async function getQuotes() {
+/* window.onload = async function () {
+    var apiString = "https://movie-quote-api.herokuapp.com/v1/shows/";
+    var response= await fetch(apiString);
+    var showsList= await response.json();
+    // console.log(showsList);
+
+    var slug = document.getElementById("showslug");
+
+    showsList.forEach(show => {
+        var newOption = new Option(show.name,show.slug);
+        slug.add(newOption);
+    });
+
+} */
+
+async function getMovieOrShow() {
     var apiString="https://movie-quote-api.herokuapp.com/v1/";
-    
-    var censored = document.getElementById("movieorshowoption").value;
+    var response= await fetch(apiString);
+    var showsList= await response.json();
+    var shows = ["mindhunter", "true-detective", "soprano", "the-wire", "sillicon-valley", "the-office", "space-force", "fargo", "fargo-s04", "fargo-s03", "ozark", "lucifer"]
+    var slug = document.getElementById("slug");
+
+    var typeSelected = document.getElementById("movieorshow");
+
+    if (typeSelected = "show")
+
+    showsList.forEach(show => {
+        var newOption = new Option(show.name,show.slug);
+        slug.add(newOption);
+    });    
+
 
     apiString=apiString + censored;  //convert user input to string
     console.log (apiString);
     var response= await fetch(apiString);
 
     var responseJson=await response.json();
-
-    document.getElementById("theQuote").innerHTML = JSON.stringify(responseJson.quote);
-
-    document.getElementById("theRole").innerHTML = JSON.stringify(responseJson.role);
-    
-    document.getElementById("theSelection").innerHTML = JSON.stringify(responseJson.show);
-
-    return true;
 }
-async function getShowQuote() {
-    var apiString="https://movie-quote-api.herokuapp.com/v1/";
+
+async function loadDropDown() {
+    var apiString = "https://movie-quote-api.herokuapp.com/v1/shows/";
+    var response= await fetch(apiString);
+    var showsList= await response.json();
+
+    var slug = document.getElementById("showslug");
+
+    showsList.forEach(show => {
+        var newOption = new Option(show.name,show.slug);
+        slug.add(newOption);
+    });
+}
+async function getQuote() {
+    var apiString="https://movie-quote-api.herokuapp.com/v1/shows/";
     
     var slug = document.getElementById("showslug").value;
 
