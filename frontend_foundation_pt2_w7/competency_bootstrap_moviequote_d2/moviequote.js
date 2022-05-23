2ff9da080b7570c9e96ea66a2ae35f307694e4ef
Melissa Tapia <mtapia.dpl@gmail.com>
2022-05-23 16:50:11 -0500

    calculator console app working

--- frontend_foundation_pt2_w7/competency_bootstrap_moviequote_d2/moviequote.js
+++ frontend_foundation_pt2_w7/competency_bootstrap_moviequote_d2/moviequote.js
@@ -1,3 +1,41 @@
+window.onload = async function () {
+    var apiString = "https://movie-quote-api.herokuapp.com/v1/shows/";
+    var response= await fetch(apiString);
+    var showsList= await response.json();
+    // console.log(showsList);
+
+    var slug = document.getElementById("programs");
+
+    showsList.forEach(show => {
+        var newOption = new Option(show.name,show.slug);
+        slug.add(newOption);
+    });
+
+}
+
+async function getQuotes() {
+    var apiString="https://movie-quote-api.herokuapp.com/v1/";
+
+    var censored = document.getElementById("censoredornot").value;
+
+    var selectedProgram= document.getElementById("programs");
+
+    apiString=apiString + censored;  //convert user input to string
+    console.log (apiString);
+    var response= await fetch(apiString);
+
+    var responseJson=await response.json();
+
+    document.getElementById("theQuote").innerHTML = JSON.stringify(responseJson.quote);
+
+    document.getElementById("theRole").innerHTML = JSON.stringify(responseJson.role);
+
+    document.getElementById("theSelection").innerHTML = JSON.stringify(responseJson.show);
+
+    return true;
+}
+
+
 /* window.onload = async function () {
     var apiString = "https://movie-quote-api.herokuapp.com/v1/shows/";
     var response= await fetch(apiString);
@@ -11,7 +49,7 @@
         slug.add(newOption);
     });
 
-} */
+}
 
 async function getMovieOrShow() {
     var apiString="https://movie-quote-api.herokuapp.com/v1/";
@@ -67,4 +105,4 @@ async function getQuote() {
     document.getElementById("theSelection").innerHTML = JSON.stringify(responseJson.show);
 
     return true;
-}
+} */
---
+++ w8/HelloWorld1000/HelloWorld1000/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld1000
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
